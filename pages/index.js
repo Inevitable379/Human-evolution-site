af85b4c32c6070dfccc3337032ea001c7e4459cd
Inevitable379 <avimangal376@gmail.com>
2025-03-24 11:29:38 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+          <div style={{ textAlign: "center", padding: "50px", backgroundColor: "#000", color: "#fff", height: "100vh" }}>
+      <h1 style={{ fontSize: "3rem", fontWeight: "bold" }}>Human: Just Another Species?</h1>
+      <p style={{ fontSize: "1.2rem", maxWidth: "600px", margin: "20px auto" }}>
+        Step into the time machine and explore the journey of human evolution.
+          </p>
+          </div>
+  );
+}
