046e899045e8fec98e0e59bdf41e47d1fa62af7d
Inevitable379 <avimangal376@gmail.com>
2025-03-25 14:31:53 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import HumanFigure from "./components/HumanFigure";
 export default function Home() {
     return (
           <div style={{ textAlign: "center", padding: "50px", backgroundColor: "#000", color: "#fff", height: "100vh" }}>
@@ -5,6 +6,6 @@ export default function Home() {
       <p style={{ fontSize: "1.2rem", maxWidth: "600px", margin: "20px auto" }}>
         Step into the time machine and explore the journey of human evolution.
           </p>
-          </div>
+          <HumanFigure />
   );
 }
