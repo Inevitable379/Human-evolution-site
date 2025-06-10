ce88c16eb1f4df57a1afe6ab65bf0e4c0ffd812a
Inevitable379 <avimangal376@gmail.com>
2025-06-10 10:20:44 +0530

    Delete pages/components directory

--- pages/components/HumanFigure.js
+++
@@ -1,8 +0,0 @@
-export default function HumanFigure() {
-    return (
-        <div>
-            <h1>Human Figure</h1>
-            <p>This is a human figure component.</p>
-        </div>
-    );
-}
